02e60846a5272edb7683c52c4e681e2a012b97b4
Jose Buelvas <jbuelvas@bancodebogota.com.co>
2020-08-24 00:54:32 -0500

    Starting base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { UserLoginComponent } from './components/user-login/user-login.component';
 
 const routes: Routes = [ {
     path: 'home',
     component: HomeComponent,
     data: { title: 'Cooper - El héroe que necesitas' }
+}, {
+    path: 'account/login',
+    component: UserLoginComponent,
+    data: {title: 'Cooper - Ingresar'}
 }, {
     path: '**',
     redirectTo: 'home',
