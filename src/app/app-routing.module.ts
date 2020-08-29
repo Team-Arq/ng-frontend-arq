369dd976e6c0dc601bc2973cd5ada38a731fede6
Sebastian Amaya <sebasamaya.gomez@gmail.com>
2020-08-29 08:33:29 -0500

    User profile and User Registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UserLoginComponent } from './components/user-login/user-login.component';
+import { UserRegitrationComponent } from './components/user-regitration/user-regitration.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 const routes: Routes = [ {
     path: 'home',
@@ -11,11 +13,20 @@ const routes: Routes = [ {
     path: 'account/login',
     component: UserLoginComponent,
     data: {title: 'Cooper - Ingresar'}
-}, {
+}
+,{
+    path: 'account/register',
+    component:UserRegitrationComponent
+} ,
+{
+    path: 'account/profile',
+    component:UserProfileComponent
+} ,{
     path: '**',
     redirectTo: 'home',
     pathMatch: 'full'
-} ];
+}
+];
 
 @NgModule( {
     imports: [ RouterModule.forRoot( routes ) ],
