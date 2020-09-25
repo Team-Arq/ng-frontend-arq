0234e5fc40adf92caa17eba69cd1fb61d3c7bcb1
Jose Buelvas <jbuelvas@bancodebogota.com.co>
2020-09-24 22:24:54 -0500

    fix

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { UserModel } from '../models/user.model';
-import { LoginModel } from '../models/login.model';
 
 @Injectable( {
   providedIn: 'root'
