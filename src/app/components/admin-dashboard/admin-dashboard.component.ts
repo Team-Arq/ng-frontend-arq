1b524784be92d15b7109e6365c1958e721a315cf
Katanac <tatisred@hotmail.com>
2020-11-07 09:53:41 -0500

    merge tatiana

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -3,14 +3,11 @@ import { SessionService } from '../../services/session.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { USER_SESSION } from '../../include/constants';
 import { RouterOutlet } from '@angular/router';
-import { ROUTER_TRANSITION } from '../../include/animations';
 
 @Component( {
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
   styleUrls: [ './admin-dashboard.component.scss' ],
-  animations: [ ROUTER_TRANSITION ],
-  changeDetection: ChangeDetectionStrategy.OnPush
 } )
 export class AdminDashboardComponent implements OnInit {
   private previousPath = '';
@@ -25,31 +22,6 @@ export class AdminDashboardComponent implements OnInit {
 
     // Check user session
     this.logged = this.session.exists( USER_SESSION );
-  }
-
-  public getPageTransition( routerOutlet: RouterOutlet ): any {
-    if ( routerOutlet.isActivated ) {
-      let transitionName = 'section';
-
-      const { path } = routerOutlet.activatedRoute.routeConfig;
-      const isSame = this.previousPath === path;
-      const isBackward = this.previousPath.startsWith( path );
-      const isForward = path.startsWith( this.previousPath );
-
-      if ( isSame ) {
-        transitionName = 'none';
-      } else if ( isBackward && isForward ) {
-        transitionName = 'initial';
-      } else if ( isBackward ) {
-        transitionName = 'backward';
-      } else if ( isForward ) {
-        transitionName = 'forward';
-      }
-
-      this.previousPath = path;
-
-      return transitionName;
-    }
   }
 
   public logout(): void {
--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -6,21 +6,21 @@ import { ServiceModel } from '../models/service.model';
 import { ServiceTypeModel } from '../models/serviceType.models';
 
 
-@Injectable({
+@Injectable( {
   providedIn: 'root',
-})
+} )
 export class ServiceService {
-  constructor(private http: HttpClient) {}
+  constructor( private http: HttpClient ) {
+  }
 
-  //services
-  registerService(body: {
+  registerService( body: {
     name: string;
     typeService: string;
     description: string;
     price: string;
-  }): Observable<ServiceModel> {
+  } ): Observable<ServiceModel> {
     return this.http.post<ServiceModel>(
-      `${environment.root_api}${environment.endpoints.register_service}`,
+      `${ environment.root_api }${ environment.endpoints.register_service }`,
       body
     );
   }
@@ -35,20 +35,19 @@ export class ServiceService {
     };
 
 
-  deleteService(body:{id:number}):Observable<any>{
+  deleteService( body: { id: number } ): Observable<any> {
     return this.http.delete<any>(
-    `${environment.root_api}${environment.endpoints.delete_services}?idService=${body.id}`);
-    };
+      `${ environment.root_api }${ environment.endpoints.delete_services }?idService=${ body.id }` );
+  }
 
 
-//types services
-  getServiceType(): Observable<{success:ServiceTypeModel[]}>{return this.http.get<{success:ServiceTypeModel[]}>(
-  ` ${environment.root_api}${environment.endpoints.get_services_types}`);
-  };
+  // types services
+  getServiceType(): Observable<{ success: ServiceTypeModel[] }> {
+    return this.http.get<{ success: ServiceTypeModel[] }>(
+      ` ${ environment.root_api }${ environment.endpoints.get_services_types }` );
+  }
 
   createServiceType( body: { nameTypeService: string, description: string } ): Observable<{ success: string }> {
     return this.http.post<{ success: string }>( `${ environment.root_api }${ environment.endpoints.create_type_service }`, body );
   }
-
-
 }
