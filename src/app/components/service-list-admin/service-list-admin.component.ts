93675ec99ae55b2a028a20523580e96e0ccc0ac1
Jose Buelvas <jbuelvas@bancodebogota.com.co>
2020-11-07 10:03:24 -0500

    admin

--- src/app/components/service-list-admin/service-list-admin.component.ts
+++ src/app/components/service-list-admin/service-list-admin.component.ts
@@ -4,71 +4,68 @@ import { FormUtils } from '../../include/form.utils';
 import { ServiceService } from '../../services/service.service';
 import { ServiceModel } from 'src/app/models/service.model';
 import { Router } from '@angular/router';
-import Swal from 'sweetalert2'; 
-import {EditServiceComponent} from '../service-list-admin/edit-service/edit-service.component'
+import Swal from 'sweetalert2';
+import { EditServiceComponent } from '../service-list-admin/edit-service/edit-service.component';
 import { from } from 'rxjs';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
-@Component({
+@Component( {
   selector: 'app-service-list-admin',
   templateUrl: './service-list-admin.component.html',
-  styleUrls: ['./service-list-admin.component.scss']
-})
+  styleUrls: [ './service-list-admin.component.scss' ]
+} )
 export class ServiceListAdminComponent implements OnInit {
 
-  //atributos
+  // atributos
   public adminService: FormGroup;
   public serviceModels: ServiceModel[];
- 
-  constructor(   private builder: FormBuilder,
-    private service: ServiceService,
-    public dialogo: MatDialog) {
-    
-   }
 
-  ngOnInit() {
+  constructor( private builder: FormBuilder,
+               private service: ServiceService,
+               public dialogo: MatDialog ) {
+
+  }
+
+  ngOnInit(): void {
     this.getServices();
-    var ServiceModels = new ServiceModels();
   }
 
-
-  public getServices(){
+  public getServices(): void {
     this.service.getServicesList().subscribe(
-    Response=>{
-      this.serviceModels=Response.success}
-    )   
+      Response => {
+        this.serviceModels = Response.success;
+      } );
   }
 
-  public deleteService(id:number ){
-    const t=this;
-    this.service.deleteService({id}).subscribe(
-      Response=>{
+  public deleteService( id: number ): void {
+    const t = this;
+    this.service.deleteService( { id } ).subscribe(
+      Response => {
+        // tslint:disable-next-line:no-shadowed-variable
         const t = this;
-        Swal.fire({
+        Swal.fire( {
           title: 'Eliminacion Exitosa',
           text: 'Has eliminado correctamente el servicio.',
           icon: 'success',
           showCancelButton: false,
           confirmButtonText: 'Continuar',
           allowOutsideClick: false,
-          preConfirm(inputValue: any): any {
+          preConfirm( inputValue: any ): any {
             t.getServices();
             return null;
           },
-        });
-
+        } );
       }
-    )
+    );
   }
 
-public openDialogEdit(service:ServiceModel){
-const dialogoref = this.dialogo.open(EditServiceComponent,{
-  width: '500px',
-  data: service,
-});
-dialogoref.afterClosed().subscribe(res=>{
+  public openDialogEdit( service: ServiceModel ): void {
+    const dialogoref = this.dialogo.open( EditServiceComponent, {
+      width: '500px',
+      data: service,
+    } );
+    dialogoref.afterClosed().subscribe( res => {
 
-})
-}
-
+    } );
+  }
 }
